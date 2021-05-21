ec93ec055f38da94b1cf740d6d79a151e760241e
Tanmayee P <may010602@gmail.com>
2021-05-21 19:51:47 +0530

    Added 6th and 7th, removed 20th and 30th

--- Problems20-30/digitfifthpowers.py
+++
@@ -1,7 +0,0 @@
-def digitfifthpowers(num : int)-> bool:
-    digits = [int(x) for x in str(num)]
-    return num == sum([x ** 5 for x in digits])
-def getallfifthpowers(start, limit : int) -> list:
-    allnums = list([x for x in range(start, limit) if digitfifthpowers(x) == True])
-    return sum(allnums)
-print(getallfifthpowers(10, 1000000))
